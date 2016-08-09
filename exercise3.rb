2746b3d390031039dc427d3f7a2d7a7e6aefbb86
ChrisP <600steps@gmail.com>
2016-08-09 13:05:54 -0400

    convert name variable to string

--- exercise3.rb
+++ exercise3.rb
@@ -1,12 +1,6 @@
 puts "What is your name?"
-name = gets.chomp
+name = gets.chomp.to_s
 puts "Hi #{name}."
 puts "How old are you?"
 age = gets.chomp.to_i
-puts "I am #{age + 10}."
-
-=begin
-(1..5).each do |num|
-  puts num
-end
-=end
+puts "I am #{age + 10}." #minor variation
