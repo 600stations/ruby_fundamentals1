26e36f51942f483e5eea9c508a2ee0930ae49624
ChrisP <600steps@gmail.com>
2016-08-08 23:36:02 -0400

    completed exercise1

--- exercise1.rb
+++ exercise1.rb
@@ -1,2 +1,7 @@
 puts 2 + 3
-puts 2 puts 3 puts 2 + 3
+# puts 2 puts 3 puts 2 + 3
+puts "Ada Lovelace lived for #{1852-1815} years"
+puts "Hello\t\tworld" #puts tab between words
+puts "Hello\b\b\b\b\bGoodbye world" #returns Goodbye world
+puts "Hello\rStart over world" # returns Start over world
+puts "1. Hello\n2. World"
