325d6dc980cb909f0a2e5ea62bb3f46a71a24105
ChrisP <600steps@gmail.com>
2016-08-09 13:01:43 -0400

    minor syntax changes with operators

--- exercise2.rb
+++ exercise2.rb
@@ -1,6 +1,6 @@
 # 1. How would you calculate a good tip for a 55 dollar meal?
 # Use puts to print the answer.
-puts 55*0.2 #calculate a 20% tip
+puts 55 * 0.2 #calculate a 20% tip
 # 2. Try adding a string and an integer with the + operator. What happens?
 #Find a way to convert the integer into a string first and use puts to print
 # the result.
@@ -10,4 +10,5 @@ print "hello " + 12.to_s + "\n"
 puts "The result of #{45628*7839}"
 # 4. What's the value of the expression (10 < 20 && 30 < 20) || !(10 == 11)?
 # Try figuring it out on your own before typing it in. returns true
-p (10 < 20 && 30 < 20) || !(10 == 11) # returns true
+p (10 < 20 && 30 < 20) || !(10 == 11)
+# returns true
