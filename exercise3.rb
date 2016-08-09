cfbedf06ae07646f12e6de9b2fb40c3fdadfe011
ChrisP <600steps@gmail.com>
2016-08-09 05:54:15 -0400

    completed exercise3

--- exercise3.rb
+++ exercise3.rb
@@ -1,3 +1,12 @@
 puts "What is your name?"
 name = gets.chomp
-puts "Hi #{name}"
+puts "Hi #{name}."
+puts "How old are you?"
+age = gets.chomp.to_i
+puts "I am #{age + 10}."
+
+=begin
+(1..5).each do |num|
+  puts num
+end
+=end
